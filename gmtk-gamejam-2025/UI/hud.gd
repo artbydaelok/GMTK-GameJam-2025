c6b15d9e7cc6918083e2b3220adb0d7c65afbc76
Kevin Patel <kevinpateldev@gmail.com>
2025-08-01 00:59:33 -0400

    hud

---
+++ gmtk-gamejam-2025/UI/hud.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+@onready var current_score_label: Label = $"Current Score"
+
+func _ready() -> void:
+	current_score_label.text = "Current Score " + str(0)
+	Global.on_score_update.connect(update_score)
+
+func update_score(current_score):
+	current_score_label.text = "Current Score " + str(current_score)
