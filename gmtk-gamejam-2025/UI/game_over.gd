b321ad8f51b31672a41b398e1aed80f9caed2c9f
Kevin Patel <kevinpateldev@gmail.com>
2025-08-01 19:16:55 -0400

    Add score display and turret shoot sound effect
    Displays the player's score on the game over screen. Adds a shoot sound effect to the turret, updates its scene structure, and improves shooting animation with scale changes.

--- gmtk-gamejam-2025/UI/game_over.gd
+++ gmtk-gamejam-2025/UI/game_over.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
+@onready var player_score_text: Label = $ScoreContainer/PlayerScoreText
 
+
+func _ready() -> void:
+	player_score_text.text = str(Global.current_score)
 
 func _on_restart_button_button_down() -> void:
 	get_tree().reload_current_scene()
--- gmtk-gamejam-2025/hazards/stationary/turret.gd
+++ gmtk-gamejam-2025/hazards/stationary/turret.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 
 const PROJECTILE = preload("res://hazards/projectile.tscn")
-@onready var projectile_spawn_point: Marker2D = $ProjectileSpawnPoint
+@onready var projectile_spawn_point: Marker2D = %ProjectileSpawnPoint
 @onready var shooting_animation: AnimationPlayer = $ShootingAnimation
+@onready var shoot_sfx: AudioStreamPlayer2D = $ShootSFX
 
 
 var level : Node2D
@@ -16,6 +17,7 @@ func shoot():
 	level.add_child(p)
 	p.global_position = projectile_spawn_point.global_position
 	p.linear_velocity = global_position.direction_to(projectile_spawn_point.global_position) * 125.0
-
+	shoot_sfx.play()
+	
 func _on_shoot_timer_timeout() -> void:
 	shooting_animation.play("shoot")
