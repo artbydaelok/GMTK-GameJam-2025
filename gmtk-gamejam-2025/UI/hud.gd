d09368f439ca66e5006f42a8c0e62ae2662d4e7f
Daelok <61995345+artbydaelok@users.noreply.github.com>
2025-08-01 02:06:43 -0400

    UI pack

--- gmtk-gamejam-2025/UI/hud.gd
+++ gmtk-gamejam-2025/UI/hud.gd
@@ -1,10 +1,10 @@
 extends CanvasLayer
 
-@onready var current_score_label: Label = $"Current Score"
+@onready var current_score_label: Label = %CurrentScoreLabel
 
 func _ready() -> void:
-	current_score_label.text = "Current Score " + str(0)
+	current_score_label.text = "Score: " + str(0) + " pts"
 	Global.on_score_update.connect(update_score)
 
 func update_score(current_score):
-	current_score_label.text = "Current Score " + str(current_score)
+	current_score_label.text = "Score: " + str(current_score) + " pts"
