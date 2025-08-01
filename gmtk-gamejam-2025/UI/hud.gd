0ba84f9385ec10e7c1e1c5307ffd925a67825249
Daelok <61995345+artbydaelok@users.noreply.github.com>
2025-08-01 05:32:39 -0400

    mvp

--- gmtk-gamejam-2025/UI/hud.gd
+++ gmtk-gamejam-2025/UI/hud.gd
@@ -1,10 +1,31 @@
 extends CanvasLayer
 
 @onready var current_score_label: Label = %CurrentScoreLabel
+@onready var health_container: HBoxContainer = %HealthContainer
+
+const HEART_DETAILED_EMPTY = preload("res://assets/Pixel UI & HUD/Pixel UI & HUD/Sprites/Hearts/Blue/HeartDetailed_Empty.png")
+const HEART_DETAILED_FULL = preload("res://assets/Pixel UI & HUD/Pixel UI & HUD/Sprites/Hearts/Blue/HeartDetailed_Full.png")
 
 func _ready() -> void:
 	current_score_label.text = "Score\n" + str(0) + " pts"
+	setup_health()
 	Global.on_score_update.connect(update_score)
+	Global.on_damaged.connect(update_health)
 
+func setup_health():
+	for i in range(Global.max_HP):
+		var s = TextureRect.new()
+		health_container.add_child(s)
+		s.texture = HEART_DETAILED_FULL
+		s.expand_mode = TextureRect.EXPAND_FIT_WIDTH_PROPORTIONAL
+		s.custom_minimum_size = Vector2(28, 24)
+
+func update_health(damage_amount):
+	for i in range(Global.max_HP):
+		if i > Global.HP - 1:
+			health_container.get_child(i).texture = HEART_DETAILED_EMPTY
+		else:
+			health_container.get_child(i).texture = HEART_DETAILED_FULL
+	
 func update_score(current_score):
 	current_score_label.text = "Score\n" + str(current_score) + " pts"
--- gmtk-gamejam-2025/levels/hazard_spawner.gd
+++ gmtk-gamejam-2025/levels/hazard_spawner.gd
@@ -2,6 +2,7 @@ extends Node
 @export var player : Player 
 @export var timer : Timer
 
+const HAZARD_ALERT = preload("res://hazards/hazard_alert.tscn")
 @export var possible_level_hazards : Array[PackedScene]
 
 # Called when the node enters the scene tree for the first time.
@@ -19,6 +20,15 @@ func spawn_hazard():
 	var hazard = hazard_scene.instantiate()
 	
 	var level = get_tree().get_first_node_in_group("Level")
+	
+	var alert = HAZARD_ALERT.instantiate()
+	level.add_child(alert)
+	alert.global_position = pos
+	
+	var spawn_delay : float = 1.5
+	await get_tree().create_timer(spawn_delay).timeout
+	
 	level.add_child(hazard)
 	
-	hazard.global_position = pos
+	hazard.global_position = alert.global_position
+	alert.queue_free()
