d1f32431611c1433f50aff1315d90b906ec287d2
Kevin Patel <kevinpateldev@gmail.com>
2025-08-01 01:39:08 -0400

    player death and projectiles

--- gmtk-gamejam-2025/Global.gd
+++ gmtk-gamejam-2025/Global.gd
@@ -2,12 +2,28 @@ extends Node
 
 var unlocked_double_jump := false
 var current_score : float
-var HP : int
+
+var HP : int = 3 
+var max_HP : int = 3
+var player : Player
 
 signal on_score_update(current_score : float)
 signal on_damaged(amount)
+
+func _ready() -> void:
+	HP = max_HP
+	player = get_tree().get_first_node_in_group("Player") as Player
+	on_damaged.connect(on_player_damaged)
 
 func update_score(amount : float):
 	current_score += amount
 	on_score_update.emit(current_score)
 	print("The current score is : " + str(current_score))
+
+func on_player_damaged(amount):
+	
+	HP = clamp(HP - amount, 0, max_HP)
+	
+	if HP <= 0:
+		player.visible = false
+		player.set_physics_process(false)
--- gmtk-gamejam-2025/hazards/projectile.gd
+++ gmtk-gamejam-2025/hazards/projectile.gd
@@ -1,17 +1,18 @@
 extends RigidBody2D
 
+@onready var destroy_timer: Timer = $DestroyTimer
+
+@export var damage_Amount : int = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	destroy_timer.timeout.connect(on_destroy)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
-		Global.on_damaged.emit(1)
-		
+		Global.on_damaged.emit(damage_Amount)
+		print("HIT " + body.name)
+		queue_free()
+
+func on_destroy():
+	queue_free()
