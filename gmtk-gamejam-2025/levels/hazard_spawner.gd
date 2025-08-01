37c7b68cd567a18e9be43bcd0baf77dd68aaecef
Daelok <61995345+artbydaelok@users.noreply.github.com>
2025-08-01 01:22:23 -0400

    test level

--- gmtk-gamejam-2025/levels/hazard_spawner.gd
+++ gmtk-gamejam-2025/levels/hazard_spawner.gd
@@ -4,9 +4,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	timer.timeout.connect(spawn_hazard)
+	
+func spawn_hazard():
 	pass
