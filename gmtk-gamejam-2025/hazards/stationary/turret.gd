c37a771464e19e361877850d2b748caefcaabb20
Kevin Patel <kevinpateldev@gmail.com>
2025-08-02 17:26:25 -0400

    camera shake and turret sound

--- gmtk-gamejam-2025/hazards/stationary/turret.gd
+++ gmtk-gamejam-2025/hazards/stationary/turret.gd
@@ -6,14 +6,17 @@ const PROJECTILE = preload("res://hazards/projectile.tscn")
 @onready var shooting_animation: AnimationPlayer = $ShootingAnimation
 @onready var shoot_sfx: AudioStreamPlayer2D = $ShootSFX
 
+@export var camera_shake_falloff : float = 350
 @export var max_shake: float = 10.0
 @export var shake_fade: float = 10.0
 
 var level : Node2D
 var player_camera : PlayerCamera
-
+var player 
 func _ready() -> void:
 	level = get_tree().get_first_node_in_group("Level")
+	player = get_tree().get_first_node_in_group("Player") as Player
+	
 	player_camera = get_tree().get_first_node_in_group("Camera")
 	player_camera.max_shake = max_shake
 	player_camera.shake_fade = shake_fade
@@ -23,8 +26,12 @@ func shoot():
 	level.add_child(p)
 	p.global_position = projectile_spawn_point.global_position
 	p.linear_velocity = global_position.direction_to(projectile_spawn_point.global_position) * 125.0
-	player_camera.trigger_camera_shake()
 	shoot_sfx.play()
+	
+	print(global_position.distance_to(player.global_position))
+	
+	if global_position.distance_to(player.global_position) <= camera_shake_falloff:
+		player_camera.trigger_camera_shake()
 	
 func _on_shoot_timer_timeout() -> void:
 	shooting_animation.play("shoot")
