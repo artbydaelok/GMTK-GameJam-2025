5ac98c12949ccb05cb5f5c016d5812df942917e9
ExpJellyfish <corbinv123@gmail.com>
2025-08-02 00:10:15 -0400

    Added Levels
    - Winding Path
    - U-Turn
    - Updated Spring (Instance Editable)
    - Changed Main Default Level

--- gmtk-gamejam-2025/hazards/stationary/spring.gd
+++ gmtk-gamejam-2025/hazards/stationary/spring.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @onready var spring_animation: AnimationPlayer = $SpringAnimation
+@export var spring_power = 635
 
 func _on_player_detect_body_entered(body: Node2D) -> void:
 	if body is Player:
 		#if not body.is_on_floor() and body.velocity.y > 0:
-		body.velocity.y = -550
+		body.velocity.y = spring_power * -1
 		spring_animation.play("default")
 		$SpringSFX.play()
