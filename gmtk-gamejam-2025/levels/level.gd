367b3c5af46595634514620ea09dd6f9ef9dac6d
Daelok <61995345+artbydaelok@users.noreply.github.com>
2025-08-02 17:12:18 -0400

    Adding teleporter cooldown, adding fix for collectible spawner

--- gmtk-gamejam-2025/levels/level.gd
+++ gmtk-gamejam-2025/levels/level.gd
@@ -6,16 +6,18 @@ extends Node2D
 @onready var hazard_spawner: Node = $HazardSpawner
 @onready var hazard_spawn_timer: Timer = $HazardSpawnTimer
 
+signal on_level_loop
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	level_end.body_entered.connect(on_level_end_triggered)
-
 
 func on_level_end_triggered(body):
 	if body is Player:
 		body.global_position = start_point.global_position
 		Global.update_score(15)
 		$LoopSFX.play()
+		on_level_loop.emit()
 	###
 	### Spawn new set of collectibles
 	###
