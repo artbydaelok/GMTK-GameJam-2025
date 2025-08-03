0197b9cb5477b015e7bd4a8d8b6bd34ccc8acd9c
Timothy Jeske <66494221+Gausroth@users.noreply.github.com>
2025-08-03 09:44:54 -0500

    Hud Hazzard Animations
    Added upcoming hazard animations to the hud.

--- gmtk-gamejam-2025/UI/hud.gd
+++ gmtk-gamejam-2025/UI/hud.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+class_name HUD extends CanvasLayer
 
 @onready var current_score_label: Label = %CurrentScoreLabel
 @onready var health_container: HBoxContainer = %HealthContainer
@@ -6,20 +6,25 @@ extends CanvasLayer
 
 const HEART_DETAILED_EMPTY = preload("res://assets/Pixel UI & HUD/Pixel UI & HUD/Sprites/Hearts/Blue/HeartDetailed_Empty.png")
 const HEART_DETAILED_FULL = preload("res://assets/Pixel UI & HUD/Pixel UI & HUD/Sprites/Hearts/Blue/HeartDetailed_Full.png")
+const bumber_anim = preload("res://UI/hazard_animations/bumper.tres")
+const saw_blade_anim = preload("res://UI/hazard_animations/sawblade.tres")
+const turret_anim = preload("res://UI/hazard_animations/turret.tres")
 
 var level : Node2D
+@export var is_running : bool = false
 
 func _ready() -> void:
+	level = get_tree().get_first_node_in_group("Level")
 	current_score_label.text = "Score\n" + str(0) + " pts"
 	setup_health()
 	Global.on_score_update.connect(update_score)
 	Global.on_damaged.connect(update_health)
 	
-	level = get_tree().get_first_node_in_group("Level")
 
 func _process(delta: float) -> void:
-	var hazard_progress_value = level.hazard_spawn_timer.time_left / level.hazard_spawn_timer.wait_time
-	hazard_progress_bar.value = (1 - hazard_progress_value) * 100.0
+	if is_running:
+		var hazard_progress_value = level.hazard_spawn_timer.time_left / level.hazard_spawn_timer.wait_time
+		hazard_progress_bar.value = (1 - hazard_progress_value) * 100.0
 
 func setup_health():
 	for i in range(Global.max_HP):
@@ -38,3 +43,10 @@ func update_health(damage_amount):
 	
 func update_score(current_score):
 	current_score_label.text = "Score\n" + str(current_score) + " pts"
+	
+func set_hazard_anim(hazzard : String):
+	is_running = true
+	level.hazard_spawn_timer.start()
+	if hazzard.contains("sawblade"): $MarginContainer/HBoxContainer/TextureRect/AnimatedTextureRect.sprites = saw_blade_anim
+	elif hazzard.contains("bumper"): $MarginContainer/HBoxContainer/TextureRect/AnimatedTextureRect.sprites = bumber_anim
+	elif hazzard.contains("turret"): $MarginContainer/HBoxContainer/TextureRect/AnimatedTextureRect.sprites = turret_anim
--- gmtk-gamejam-2025/levels/hazard_spawner.gd
+++ gmtk-gamejam-2025/levels/hazard_spawner.gd
@@ -1,30 +1,36 @@
 extends Node
 @export var player : Player 
 @export var timer : Timer
+@export var HUD : HUD
 
 const HAZARD_ALERT = preload("res://hazards/hazard_alert.tscn")
 @export var possible_level_hazards : Array[PackedScene]
 
+var hazard_scene
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	await get_tree().create_timer(.1).timeout
+	hazard_scene = possible_level_hazards.pick_random()
+	HUD.set_hazard_anim(hazard_scene.get_path())
 	timer.timeout.connect(spawn_hazard)
 
 func reset_timer():
 	timer.stop()
 	timer.start()
+	hazard_scene = possible_level_hazards.pick_random()
 
 func spawn_hazard():
 	var pos = player.global_position + Vector2(0, -16)
-
-	var hazard_scene = possible_level_hazards.pick_random()
 	var hazard = hazard_scene.instantiate()
-	
 	var level = get_tree().get_first_node_in_group("Level")
-	
 	var alert = HAZARD_ALERT.instantiate()
+	
 	level.add_child(alert)
 	alert.global_position = pos
 	
+	HUD.is_running = false
+	timer.stop()
 	var spawn_delay : float = 1.5
 	await get_tree().create_timer(spawn_delay).timeout
 	
@@ -32,3 +38,5 @@ func spawn_hazard():
 	
 	hazard.global_position = alert.global_position
 	alert.queue_free()
+	hazard_scene = possible_level_hazards.pick_random()
+	HUD.set_hazard_anim(hazard_scene.get_path())
