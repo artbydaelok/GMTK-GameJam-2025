f9927c9925a83ea757e8450cfb0005932890b61e
Daelok <61995345+artbydaelok@users.noreply.github.com>
2025-08-02 22:56:43 -0400

    drop loop title effect

--- gmtk-gamejam-2025/UI/main_menu.gd
+++ gmtk-gamejam-2025/UI/main_menu.gd
@@ -4,8 +4,45 @@ var level_one = preload("res://main.tscn")
 var audio = preload("res://UI/audio_settings.tscn")
 var how_to_play = preload("res://UI/how_to_play.tscn")
 
+const DEAD_REVOLVER_DISPLAY = preload("res://assets/Fonts/Dead Revolver Typeface/Dead Revolver Typeface/Fonts/Display/OTF/DeadRevolverDisplay.otf")
+const DEAD_REVOLVER_DISPLAY_OUTLINED = preload("res://assets/Fonts/Dead Revolver Typeface/Dead Revolver Typeface/Fonts/Display/OTF/DeadRevolverDisplayOutlined.otf")
+const DEAD_REVOLVER_ARCADE = preload("res://assets/Fonts/Dead Revolver Typeface/Dead Revolver Typeface/Fonts/Arcade/OTF/DeadRevolverArcade.otf")
+const DEAD_REVOLVER_ARCADE_OUTLINED = preload("res://assets/Fonts/Dead Revolver Typeface/Dead Revolver Typeface/Fonts/Arcade/OTF/DeadRevolverArcadeOutlined.otf")
+const DEAD_REVOLVER_GAME = preload("res://assets/Fonts/Dead Revolver Typeface/Dead Revolver Typeface/Fonts/Game/OTF/DeadRevolverGame.otf")
+
+@onready var game_title_drop: Label = %GameTitleDrop
+@onready var game_title_loop: Label = %GameTitleLoop
+
+var fonts : Array[Font]
+@onready var font_timer: Timer = $FontTimer
+var current_font : Font 
+
 func _ready() -> void:
+	fonts = [
+			DEAD_REVOLVER_ARCADE, 
+			DEAD_REVOLVER_ARCADE_OUTLINED,
+			DEAD_REVOLVER_GAME,
+			DEAD_REVOLVER_DISPLAY, 
+			DEAD_REVOLVER_DISPLAY_OUTLINED
+			]
 	focus()
+	
+func change_font():
+	var remaining_fonts = fonts.duplicate()
+	
+	if current_font != null:
+		remaining_fonts.erase(current_font)
+		
+	var new_font = remaining_fonts.pick_random()
+	current_font = new_font
+	
+	game_title_drop.add_theme_font_override("font", new_font)
+	
+	await get_tree().create_timer(font_timer.wait_time * 0.5).timeout
+	
+	game_title_loop.add_theme_font_override("font", new_font)
+	#game_title_drop.add_theme_font_override("font", fonts.pick_random())
+	#game_title_loop.add_theme_font_override("font", fonts.pick_random())
 	
 func focus() -> void:
 	$Panel/MarginContainer/VBoxContainer2/PlayButton.grab_focus()
@@ -39,3 +76,7 @@ func _on_sluggerpunk_studios_button_pressed() -> void:
 
 func _on_discord_button_pressed() -> void:
 	OS.shell_open("https://discord.gg/HHvPB36fEd")
+
+
+func _on_font_timer_timeout() -> void:
+	change_font()
