d1878d6d1a4ea09a49d6b3cbcd7d68759aea7fc6
Timothy Jeske <66494221+Gausroth@users.noreply.github.com>
2025-08-03 11:33:24 -0500

    music player

--- gmtk-gamejam-2025/music player/music_player.gd
+++ gmtk-gamejam-2025/music player/music_player.gd
@@ -6,6 +6,8 @@ var current_track_index := 0
 func _ready() -> void:
 	var level = get_tree().get_first_node_in_group("Level")
 	level.on_level_loop.connect(progress_next)
+	await get_tree().create_timer(.1).timeout
+	$".".play()
 
 
 func progress_next():
