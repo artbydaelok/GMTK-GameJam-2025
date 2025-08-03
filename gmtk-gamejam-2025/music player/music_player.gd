2a9d46db4c73fa6844aecf2bc4965ee9ec958f0e
Daelok <61995345+artbydaelok@users.noreply.github.com>
2025-08-03 09:00:07 -0400

    Adding music

---
+++ gmtk-gamejam-2025/music player/music_player.gd
@@ -0,0 +1,19 @@
+extends AudioStreamPlayer
+
+var current_track_index := 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var level = get_tree().get_first_node_in_group("Level")
+	level.on_level_loop.connect(progress_next)
+
+
+func progress_next():
+	if current_track_index == 3:
+		return
+	
+	current_track_index += 1
+	
+	var s : AudioStreamSynchronized = stream
+	s.set_sync_stream_volume(current_track_index, 0)
+	
